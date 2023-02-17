ea4a70ad7057664b3c737285f83aa2e943cda82e
robert.hoang <hoangtiendat.thidaihoc@gmail.com>
2023-02-17 14:50:52 +0700

    update detail view

--- HelloWorld/View/CustomList.swift
+++ HelloWorld/View/CustomList.swift
@@ -10,26 +10,8 @@ import SwiftUI
 struct CustomList: View {
     
     @State var restaurants = [
-        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location:"Hong Kong", image: "cafedeadend", isFavorite: false),
-        Restaurant(name: "Homei", type: "Cafe", location: "Hong Kong", image: "homei", isFavorite: false),
-        Restaurant(name: "Teakha", type: "Tea House", location: "Hong Kong", image: "teakha", isFavorite: false),
-        Restaurant(name: "Cafe loisl", type: "Austrian / Causual Drink", location: "Hong Kong", image: "cafeloisl", isFavorite: false),Restaurant(name: "Petite Oyster", type: "French", location: "Hong Kong", image: "petiteoyster", isFavorite: false),
-        Restaurant(name: "For Kee Restaurant", type: "Bakery", location: "Hong Kong", image: "forkee", isFavorite: false),
-        Restaurant(name: "Po's Atelier", type: "Bakery", location: "Hong Kong", image: "posatelier", isFavorite: false),
-        Restaurant(name: "Bourke Street Backery", type: "Chocolate", location: "Sydney", image: "bourkestreetbakery", isFavorite: false),
-        Restaurant(name: "Haigh's Chocolate", type: "Cafe", location: "Sydney", image: "haigh", isFavorite: false),
-        Restaurant(name: "Palomino Espresso", type: "American / Seafood", location: "Sydney", image: "palomino", isFavorite: false),
-        Restaurant(name: "Upstate", type: "American", location: "New York", image: "upstate", isFavorite: false),
-        Restaurant(name: "Traif", type: "American", location: "New York", image: "traif", isFavorite: false),
-        Restaurant(name: "Graham Avenue Meats", type: "Breakfast & Brunch", location: "New York", image: "graham", isFavorite: false),
-        Restaurant(name: "Waffle & Wolf", type: "Coffee & Tea", location: "New York", image: "waffleandwolf", isFavorite: false),
-        Restaurant(name: "Five Leaves", type: "Coffee & Tea", location: "New York", image: "fiveleaves", isFavorite: false),
-        Restaurant(name: "Cafe Lore", type: "Latin American", location: "New York", image: "cafelore", isFavorite: false),
-        Restaurant(name: "Confessional", type: "Spanish", location: "New York", image: "confessional", isFavorite: false),
-        Restaurant(name: "Barrafina", type: "Spanish", location: "London", image: "barrafina", isFavorite: false),
-        Restaurant(name: "Donostia", type: "Spanish", location: "London", image: "donostia", isFavorite: false),
-        Restaurant(name: "Royal Oak", type: "British", location: "London", image: "royaloak", isFavorite: false),
-        Restaurant(name: "CASK Pub and Kitchen", type: "Thai", location: "London", image: "cask", isFavorite: false)
+        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", image: "cafedeadend", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafelatte, piccolo and many more. Come over and enjoy a great meal.", phone: "232-923423", isFavorite: false),
+        Restaurant(name: "Homei", type: "Cafe", location: "Shop B, G/F, 22-24A Tai Ping San Street SOHO, Sheung Wan, Hong Kong", image: "homei", description: "A little gem hidden at the corner of the street is nothing butfantastic! This place is warm and cozy. We open at 7 every morning except Sunday, and close at 9 PM. We offer a variety of coffee drinks and specialties including lattes, cappuccinos, teas, and more. We serve breakfast, lunch, and dinner in an airy open setting. Come over, have a coffee and enjoy a chit-chat with our baristas.", phone: "348-233423", isFavorite: false),
     ]
     
     var body: some View {
--- HelloWorld/View/RestaurantDetailView.swift
+++ HelloWorld/View/RestaurantDetailView.swift
@@ -12,44 +12,80 @@ struct RestaurantDetailView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        ZStack(alignment: .top) {
-            Image(restaurant.image)
-                .resizable()
-                .scaledToFill()
-                .frame(minWidth: 0, maxWidth: .infinity)
-            .ignoresSafeArea()
-            
-            Color.gray
-                .frame(height: 100)
-                .opacity(0.8)
-                .cornerRadius(20)
-                .padding()
-                .overlay {
-                    VStack(spacing: 5) {
-                        Text(restaurant.name)
-                        Text(restaurant.type)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Image(restaurant.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 445)
+                    .overlay {
+                        VStack {
+                            Image(systemName: restaurant.isFavorite ? "heart.fill" : "heart")
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+                                .padding()
+                                .font(.system(size: 30))
+                                .foregroundColor(restaurant.isFavorite ? .pink : .white)
+                                .padding(.top, 40)
+                                .onTapGesture {
+                                    
+                                }
+                             
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(restaurant.name)
+                                    .font(.system(.largeTitle))
+                                    .bold()
+                                
+                                Text(restaurant.type)
+                                    .font(.system(.headline, design: .rounded))
+                                    .padding(.all, 5)
+                                    .background(.black)
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
+                            .foregroundColor(.white)
+                            .padding()
+                        }
+                    }
+                Text(restaurant.description)
+                    .padding()
+                
+                HStack(alignment: .top, spacing: 10) {
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("ADDRESS")
+                            .font(.system(.headline, design: .rounded))
                         Text(restaurant.location)
                     }
-                    .font(.system(.headline, design: .rounded))
-                    .foregroundColor(.white)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("PHONE")
+                            .font(.system(.headline, design: .rounded))
+                        Text(restaurant.phone)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                 }
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button {
-                    dismiss()
-                } label: {
-                    Text("\(Image(systemName: "chevron.left")) \(restaurant.name)")
-                }
+                .padding(.horizontal)
+            }
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("\(Image(systemName: "chevron.left")) \(restaurant.name)")
+                    }
 
+                }
             }
         }
+        .ignoresSafeArea()
+       
     }
 }
 
 struct RestaurantDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", image: "cafedeadend", isFavorite: true))
+        RestaurantDetailView(restaurant: Restaurant(name: "Homei", type: "Cafe", location: "Shop B, G/F, 22-24A Tai Ping San Street SOHO, Sheung Wan, Hong Kong", image: "homei", description: "A little gem hidden at the corner of the street is nothing butfantastic! This place is warm and cozy. We open at 7 every morning except Sunday, and close at 9 PM. We offer a variety of coffee drinks and specialties including lattes, cappuccinos, teas, and more. We serve breakfast, lunch, and dinner in an airy open setting. Come over, have a coffee and enjoy a chit-chat with our baristas.", phone: "348-233423", isFavorite: false))
     }
 }
